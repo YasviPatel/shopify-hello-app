ef9f4d5e2071e17a1a504e124776146734141119
YasviPatel <79090119+YasviPatel@users.noreply.github.com>
2026-09-20 21:23:14 +0530

    chore: updated the text of the admin home page

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,19 +1,18 @@
 export default function Index() {
   return (
     <s-page heading="Hello App">
-      <s-section heading="👋 Hello World!">
+      <s-section heading="Learning Shopify">
         <s-paragraph>
-          Welcome to your custom Shopify App! This app is successfully connected
-          and running in your demo store.
+          Hello World
         </s-paragraph>
       </s-section>
 
       <s-section slot="aside" heading="App Status">
         <s-paragraph>
-          Environment: <strong>Production / Demo Store</strong>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae qui eos esse sint molestiae, dicta placeat, vitae reiciendis excepturi nemo repellendus est. Ad quasi voluptates quo, corrupti distinctio sequi perspiciatis.
         </s-paragraph>
         <s-paragraph>
-          Host: <strong>AWS Lightsail</strong>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iste minima ut magnam. Incidunt veritatis distinctio ab id omnis recusandae quos quisquam adipisci vitae molestias nulla debitis, ullam mollitia perferendis.
         </s-paragraph>
       </s-section>
     </s-page>
